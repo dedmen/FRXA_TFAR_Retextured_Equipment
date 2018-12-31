95f10f901a6706ca81535a1c0cba537e49848e84
Dedmen Miller <dedmen@users.noreply.github.com>
2018-12-31 22:17:44 +0100

    Config Cleanup

--- FRXA_TFAR_Extra_Retextured_Equipment/config.cpp
+++ FRXA_TFAR_Extra_Retextured_Equipment/config.cpp
@@ -1,13 +1,13 @@
 class CfgMods
 {
-	class Mod_Base; // External Class Reference
-	class FRXAs_TFAR_Extra_Retextured_Equipment_DLC : Mod_Base
+	class Mod_Base;
+	class FRXAs_TFAR_Extra_Retextured_Equipment_DLC: Mod_Base
 	{
 		logo = "FRXA_TFAR_Extra_Retextured_Equipment\data\ui\FRXAs_TFAR_Extra_Retextured_Equipment_logo.paa";
 	};
 };
 
-class CfgPatches // Addon Dependants
+class CfgPatches
 {
 	class FRXA_TFAR_Extra_Retextured_Equipment
 	{
@@ -15,171 +15,116 @@ class CfgPatches // Addon Dependants
 		weapons[] = {};
 		requiredVersion = 0.1;
 		requiredAddons[] = {"task_force_radio_items"};
-		authors[] = {"freexavier", "dedmen"};
+		authors[] = {"freexavier","dedmen"};
 	};
 };
 
-class CfgVehicles // Item config
+class CfgVehicles
 {
 	class tf_rt1523g;
 	class tf_rt1523g_big;
-
-	class FRXA_tf_rt1523g_UCP: tf_rt1523g // UCP, Small
+	class FRXA_tf_rt1523g_UCP: tf_rt1523g
 	{
 		scope = 2;
 		scopeArsenal = 2;
 		scopeCurator = 2;
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) [UCP]";
-		descriptionShort = "RT-1523G (ASIP) long range radio 20km";
 		DLC = "FRXAs_TFAR_Extra_Retextured_Equipment_DLC";
-		maximumLoad = 50;
-		mass = 80;
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_prc117g_UCP_co.paa"};
-		model = "\task_force_radio_items\models\clf_prc117g_ap";
 	};
-	
-	class FRXA_tf_rt1523g_big_UCP: tf_rt1523g_big // UCP,Big
+	class FRXA_tf_rt1523g_big_UCP: tf_rt1523g_big
 	{
 		scope = 2;
 		scopeArsenal = 2;
 		scopeCurator = 2;
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) Big [UCP]";
-		descriptionShort = "RT-1523G (ASIP) Big long range radio 20km";
 		DLC = "FRXAs_TFAR_Extra_Retextured_Equipment_DLC";
-		maximumLoad = 160;
-		mass = 160;
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_nicecomm2_UCP_co.paa"};
-		model = "\task_force_radio_items\models\clf_nicecomm2";
 	};
-	
-	class FRXA_tf_rt1523g_CTRG: FRXA_tf_rt1523g_UCP // CTRG Grey, Small
+	class FRXA_tf_rt1523g_CTRG: FRXA_tf_rt1523g_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) [CTRG]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_prc117g_CTRG_co.paa"};
-		model = "\task_force_radio_items\models\clf_prc117g_ap";
 	};
-	
-	class FRXA_tf_rt1523g_big_CTRG: FRXA_tf_rt1523g_big_UCP // CTRG Grey, Big
+	class FRXA_tf_rt1523g_big_CTRG: FRXA_tf_rt1523g_big_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) Big [CTRG]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_nicecomm2_CTRG_co.paa"};
-		model = "\task_force_radio_items\models\clf_nicecomm2";
 	};
-	
-	class FRXA_tf_rt1523g_CTRG_Green: FRXA_tf_rt1523g_UCP // CTRG Green, Small
+	class FRXA_tf_rt1523g_CTRG_Green: FRXA_tf_rt1523g_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) [CTRG Green]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_prc117g_CTRG_Green_co.paa"};
-		model = "\task_force_radio_items\models\clf_prc117g_ap";
 	};
-	
-	class FRXA_tf_rt1523g_big_CTRG_Green: FRXA_tf_rt1523g_big_UCP // CTRG Green, Big
+	class FRXA_tf_rt1523g_big_CTRG_Green: FRXA_tf_rt1523g_big_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) Big [CTRG Green]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_nicecomm2_CTRG_Green_co.paa"};
-		model = "\task_force_radio_items\models\clf_nicecomm2";
 	};
-	
-	class FRXA_tf_rt1523g_MARPAT_Woodland: FRXA_tf_rt1523g_UCP // MARPAT Woodland, Small
+	class FRXA_tf_rt1523g_MARPAT_Woodland: FRXA_tf_rt1523g_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) [MARPAT Woodland]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_prc117g_MARPAT_Woodland_co.paa"};
-		model = "\task_force_radio_items\models\clf_prc117g_ap";
 	};
-	
-	class FRXA_tf_rt1523g_big_MARPAT_Woodlaand: FRXA_tf_rt1523g_big_UCP //  MARPAT Woodland, Big
+	class FRXA_tf_rt1523g_big_MARPAT_Woodlaand: FRXA_tf_rt1523g_big_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) Big [MARPAT Woodland]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_nicecomm2_MARPAT_Woodland_co.paa"};
-		model = "\task_force_radio_items\models\clf_nicecomm2";
 	};
-	
-	class FRXA_tf_rt1523g_MARPAT_Desert: FRXA_tf_rt1523g_UCP // MARPAT Desert, Small
+	class FRXA_tf_rt1523g_MARPAT_Desert: FRXA_tf_rt1523g_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) [MARPAT Desert]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_prc117g_MARPAT_Desert_co.paa"};
-		model = "\task_force_radio_items\models\clf_prc117g_ap";
 	};
-	
-	class FRXA_tf_rt1523g_big_MARPAT_Desert: FRXA_tf_rt1523g_big_UCP //  MARPAT Desert, Big
+	class FRXA_tf_rt1523g_big_MARPAT_Desert: FRXA_tf_rt1523g_big_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) Big [MARPAT Desert]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_nicecomm2_MARPAT_Desert_co.paa"};
-		model = "\task_force_radio_items\models\clf_nicecomm2";
 	};
-	
-	class FRXA_tf_rt1523g_Black: FRXA_tf_rt1523g_UCP// Black, Small
+	class FRXA_tf_rt1523g_Black: FRXA_tf_rt1523g_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) [Black]";
-		DLC = "FRXAs_TFAR_Extra_Retextured_Equipment_DLC";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_prc117g_Black_co.paa"};
-		model = "\task_force_radio_items\models\clf_prc117g_ap";
 	};
-	
-	class FRXA_tf_rt1523g_big_Black: FRXA_tf_rt1523g_big_UCP //  Black, Big
+	class FRXA_tf_rt1523g_big_Black: FRXA_tf_rt1523g_big_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) Big [Black]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_nicecomm2_Black_co.paa"};
-		model = "\task_force_radio_items\models\clf_nicecomm2";
 	};
-	
-	class FRXA_tf_rt1523g_M81: FRXA_tf_rt1523g_UCP// M81, Small
+	class FRXA_tf_rt1523g_M81: FRXA_tf_rt1523g_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) [M81]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_prc117g_M81_co.paa"};
-		model = "\task_force_radio_items\models\clf_prc117g_ap";
 	};
-	
-	class FRXA_tf_rt1523g_big_M81: FRXA_tf_rt1523g_big_UCP //  M81, Big
+	class FRXA_tf_rt1523g_big_M81: FRXA_tf_rt1523g_big_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) Big [M81]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_nicecomm2_M81_co.paa"};
-		model = "\task_force_radio_items\models\clf_nicecomm2";
 	};
-	
-	class FRXA_tf_rt1523g_Ranger_Green: FRXA_tf_rt1523g_UCP// Ranger Green, Small
+	class FRXA_tf_rt1523g_Ranger_Green: FRXA_tf_rt1523g_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) [Ranger Green]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_prc117g_Ranger_Green_co.paa"};
-		model = "\task_force_radio_items\models\clf_prc117g_ap";
 	};
-	
-	class FRXA_tf_rt1523g_big_Ranger_Green: FRXA_tf_rt1523g_big_UCP //  Ranger Green, Big
+	class FRXA_tf_rt1523g_big_Ranger_Green: FRXA_tf_rt1523g_big_UCP
 	{
 		author = "Raspu, Gandi, Nkey, freexavier(FRXA)";
 		displayName = "RT-1523G (ASIP) Big [Ranger Green]";
-		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\FRXA_TFAR_Extra_Retextured_Equipment\data\clf_nicecomm2_Ranger_Green_co.paa"};
-		model = "\task_force_radio_items\models\clf_nicecomm2";
 	};
 };
